557c5a1680e552fd25b142f5aefbefefee6be9a2
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-05 20:43:26 -0500

    conversion added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -7,13 +7,17 @@ then
 echo "ERROR:: You must have sudo sccess to execute this script" 
 exit 1 
 fi 
+dnf list installed mysql
+if [ $? -ne 0 ]
  dnf install mysql -y
-
  if [ $? -ne 0 ]
-
- then echo "Installing MYSQL is failure"
+then 
+ echo "Installing MYSQL is failure"
 exit 1 
-
 else 
 echo "Installing MYSQL is success" 
+fi 
+
+else
+echo "MySQL is already installed"
 fi 
