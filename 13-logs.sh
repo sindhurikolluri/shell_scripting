65aca43d4bfb79917cc4a133d8bdbbb8b1a9b6f6
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-05 23:06:29 -0500

    conversion added

--- 13-logs.sh
+++ 13-logs.sh
@@ -21,6 +21,8 @@ VALIDATE()
 }
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if[ $USERID -ne 0 ]
 then
     echo "ERROR:: You must have sudo access to execute this script"
     exit 1 #other than 0
