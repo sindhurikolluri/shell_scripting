8fccdd3187e2b3938285cc6da3b6dc65619bbcd3
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-05 19:02:46 -0500

    conversion added

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,6 +4,6 @@ PERSON1=$1
 PERSON2=$2
 
 echo "$PERSON1:: Hi $PERSON2"
-echo "Robet:: Hello $PERSON1"
+echo "$PERSON2:: Hello $PERSON1"
 echo "$PERSON1:: How are you doing?"
 echo "$PERSON2:: Iam good. How are you?"
