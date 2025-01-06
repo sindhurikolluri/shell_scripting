712d99c2aa3ad7a6c36ce9a3ecaefa7e39ec629b
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-05 20:06:25 -0500

    conversion added

---
+++ 08-special-vars.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Present Working Directory $PWD"
+echo "Home directory of current user: $HOME"
+echo "Which user is running this script: $USER"
+echo "Process id of current script: $$"
+sleep 60 &
+echo "Process id of last command in background: $!"
