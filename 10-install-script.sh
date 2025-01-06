f320e7e81b144eb3811b3f79de054488f00c6a60
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-05 20:46:11 -0500

    conversion added

--- 10-install-script.sh
+++ 10-install-script.sh
@@ -1,23 +1,26 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
-then 
-echo "ERROR:: You must have sudo sccess to execute this script" 
-exit 1 
-fi 
-dnf list installed mysql
-if [ $? -ne 0 ]
- dnf install mysql -y
- if [ $? -ne 0 ]
-then 
- echo "Installing MYSQL is failure"
-exit 1 
-else 
-echo "Installing MYSQL is success" 
-fi 
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    if [ $? -ne 0 ]
+    then
+        echo "Installing MySQL ... FAILURE"
+        exit 1
+    else
+        echo "Installing MySQL ... SUCCESS"
+    fi
 else
-echo "MySQL is already installed"
-fi 
+    echo "MySQL is already ... INSTALLED"
+fi
