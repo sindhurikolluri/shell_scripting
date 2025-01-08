6b9ffcb66ed29cef5c9dd091e8fd9993cc0f5208
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-08 17:28:05 -0500

    session-12

---
+++ 15-loops.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+LOGS_FOLDER="/var/log/shellscript_logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE()
+{
+   if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ...$G SUCCESS"
+    fi  
+}
+
+echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
+fi
+
+for package in $@
+do 
+dnf list intalled $package &>>$LOG_FILE_NAME
+if[ $? -ne 0 ]
+then 
+dnf install $package -y &>>$LOG_FILE_NAME
+VALIDATE $? "Installing $package"
+else
+eco -e "$package is already $Y installed"
+fi
+done
