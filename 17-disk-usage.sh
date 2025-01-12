19b740280ffb101e0a93bbc3f5cf14bc87eca5ac
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-12 18:36:28 -0500

    scheduling the deletes of the logs

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -4,9 +4,16 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 
+MSG=" "
 while read -r line 
 do 
       USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
       PARTITION=$(echo $line | awk -F " " '{print $NF}')
-      echo "Partition: $PARTITION, usage: $USAGE"
+      #echo "Partition: $PARTITION, usage: $USAGE"
+      if [ $USAGE -go $DISK_THRESHOLD ]
+      then
+          MSG="High Disk Usage on partition: $PARTITION Usage is: $USAGE"
+       
 done <<< $DISK_USAGE
+
+echo "Message: $MSG"
