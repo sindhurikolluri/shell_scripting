1ff6a2e12cbb9b0231b65725f2df206a82d62431
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-12 12:34:36 -0500

    scheduling the deletes of the logs

--- 16-backup.sh
+++ 16-backup.sh
@@ -22,16 +22,35 @@ VALIDATE(){
     echo -e "$2......$G SUCCESS $N"
     fi
 }
+#To check if the user is giving is giving the correct input
+
 USAGE()
 {
     echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(optional)>"
     exit 1
 }
+
 mkdir -p /home/ec2-user/shellscript-logs/
+
+#Checking if the no of arguments provided is correct 
 
 if [ $# -lt 2 ]
 then
 USAGE
+fi
+
+#checking if the given source directory exist
+if [ ! -d $SOURCE_DIR ]
+then 
+    echo -e "$SOURCE_DIR Does not exist....please check"
+    exit 1
+fi
+
+#checking if the destination directory exist
+if [ ! -d $DEST_DIR ]
+then 
+    echo -e "$DEST_DIR Does not exist....please check"
+    exit 1
 fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
