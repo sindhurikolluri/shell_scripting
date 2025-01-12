3ef1020d87176f352992e99101d68f80eb437026
sindhurikolluri <sindhurikolluri2020@gmail.com>
2025-01-12 16:47:30 -0500

    scheduling the deletes of the logs

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -6,7 +6,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 
 while read -r line 
 do 
-      USAGE=$(echo $line | awk -F " " '{print $6F}')
+      USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
       PARTITION=$(echo $line | awk -F " " '{print $NF}')
       echo "Partition: $PARTITION, usage: $USAGE"
 done <<< $DISK_USAGE
